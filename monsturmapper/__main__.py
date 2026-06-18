0a297de513a3b4c942553d9a826a03ad0bae49ba
Cameron Bodine <bodine.cs@gmail.com>
2026-06-18 05:12:21 -0400

    Revert main

--- monsturmapper/__main__.py
+++ monsturmapper/__main__.py
@@ -11,23 +11,17 @@ SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 PACKAGE_DIR = os.path.dirname(SCRIPT_DIR)
 sys.path.append(PACKAGE_DIR)
 
-# Set GHOSTVISION utils dir
+# Set MONSTURMAPPER utils dir
 USER_DIR = os.path.expanduser('~')
 MM_UTILS_DIR = os.path.join(USER_DIR, '.monsturmapper')
 if not os.path.exists(MM_UTILS_DIR):
     os.makedirs(MM_UTILS_DIR)
 
 # Default function to run
-
-# print("Parameters passed to MonSturMapper:")
-# print(sys.argv[2])
-# sys.exit()
-
 if len(sys.argv) == 1:
     to_do = 'gui'
 else:
-    to_do = sys.argv[1:]
-
+    to_do = sys.argv[1]
 
 #=======================================================================
 def main(process):
@@ -38,39 +32,10 @@ def main(process):
     print("\n\nMonSturMapper v{}".format(__version__))
 
     # Launch GUI
-    if process[0] == 'gui':
+    if process == 'gui':
         print('\n\nLaunching MonSturMapper gui...\n\n')
 
-        # from monsturmapper.gui_main import gui
-
-    elif process[0] == 'gui_batch':
-        print('\n\nLaunching MonSturMapper batch gui...\n\n')
-        
-        # from monsturmapper.gui_main import gui_batch
-
-        # gui_batch()
-
-    elif process[0] == 'mask_shadows':
-
-        print('\n\nRemoving transect shadows...\n\n')
-
-        try:
-            transect_path = to_do[1]
-        except:
-            print('\n\nNo transect path provided for shadow removal.\n\n')
-            sys.exit(1)
-
-        from monsturmapper.test_utilities import remove_transect_shadows
-
-
-        remove_transect_shadows(transect_path)
-
-    else:
-        print('\n\nUnknown process: {}'.format(process))
-
-        print('Valid options are: "gui" or "gui_batch"')
-
-        sys.exit(1)
+        from monsturmapper.gui_main import gui
 
     return
 
